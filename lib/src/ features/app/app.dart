c29d8fd15aa4cf361b2d6fd48d6e0454689a6315
artem-zaitsev <zaitsev_art@inbox.ru>
2022-04-05 17:28:23 +0300

    подключил шрифты +  убрал лишнее + докинул ассеты

---
+++ lib/src/ features/app/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class PickOfficeApp extends StatelessWidget {
+  const PickOfficeApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Providing a restorationScopeId allows the Navigator built by the
+      // MaterialApp to restore the navigation stack when a user leaves and
+      // returns to the app after it has been killed while running in the
+      // background.
+      restorationScopeId: 'app',
+
+      // Provide the generated AppLocalizations to the MaterialApp. This
+      // allows descendant Widgets to display the correct translations
+      // depending on the user's locale.
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), 
+        Locale('ru', ''), 
+      ],
+
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.appTitle,
+
+      // Define a light and dark color theme. Then, read the user's
+      // preferred ThemeMode (light, dark, or system default) from the
+      // SettingsController to display the correct theme.
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+    );
+  }
+}
