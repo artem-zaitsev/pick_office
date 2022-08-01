0b8275a6be0c45de5a44036e2ed052b388677bfb
artem-zaitsev <zaitsev_art@inbox.ru>
2022-08-01 19:01:50 +0300

    animation from 2nd stream

--- lib/src/features/animations/ui/button/button_screen.dart
+++ lib/src/features/animations/ui/button/button_screen.dart
@@ -25,74 +25,158 @@ class _Button extends StatefulWidget {
 }
 
 class _ButtonState extends State<_Button>
-    with SingleTickerProviderStateMixin<_Button> {
+    with TickerProviderStateMixin<_Button> {
   late final AnimationController _controller;
 
-  late final Animation<Offset> _offset;
-  late final Animation<double> _rotation;
+  late final AnimationController _scaleController;
+
+  late final Animation<double> _radius;
+  late final Animation<double> _scaleOut;
+  late final Animation<double> _scaleIn;
+
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      value: 0.5,
       vsync: this,
       duration: Duration(
-        milliseconds: 800,
+        milliseconds: 400,
       ),
-    )..repeat(reverse: true);
+    )..repeat();
 
-    _offset = Tween<Offset>(
-      begin: const Offset(-10, 0),
-      end: const Offset(10, 0),
-    ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    _scaleController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 600,
+      ),
     );
 
-    _rotation = Tween<double>(begin: -pi / 2, end: pi / 2).animate(_controller);
+    _radius = Tween<double>(begin: 0, end: 3).animate(_controller);
+
+    _scaleOut = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: _scaleController,
+        curve: Interval(
+          0,
+          0.5,
+        ),
+      ),
+    );
+
+    _scaleIn = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _scaleController,
+        curve: Interval(
+          0.5,
+          1,
+        ),
+      ),
+    );
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _scaleController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (ctx, child) {
-        return Transform.rotate(
-          angle: _rotation.value,
-          child: Transform.translate(
-            offset: _offset.value,
-            child: child,
-          ),
-        );
-      },
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-          color: Color(0xFF8DEBE5),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 3),
-              color: Colors.black45,
-              blurRadius: 10,
-            )
-          ],
-        ),
-        clipBehavior: Clip.hardEdge,
-        width: 250,
-        height: 56,
-        child: Material(
-          type: MaterialType.transparency,
-          child: InkWell(
-            onTap: () {},
-            child: const Center(child: Text('TAP ME')),
+    return Stack(
+      children: [
+        Center(
+          child: ScaleTransition(
+            scale: _scaleIn,
+            child: Center(
+              child: GestureDetector(
+                onTap: _handleTap,
+                child: _Loader(),
+              ),
+            ),
           ),
         ),
+        Center(
+          child: ScaleTransition(
+            scale: _scaleOut,
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (ctx, child) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    color: Color(0xFF8DEBE5),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 3),
+                        color: Colors.black45,
+                        blurRadius: 10,
+                      )
+                    ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  width: 250,
+                  height: 56,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        colors: [
+                          Colors.white,
+                          Colors.white.withOpacity(0.07),
+                        ],
+                        radius: _radius.value,
+                      ),
+                    ),
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: InkWell(
+                        onTap: _handleTap,
+                        child: const Center(child: Text('TAP ME')),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _handleTap() {
+    if (_isLoading) {
+      _scaleController.reverse();
+    } else {
+      _scaleController.forward();
+    }
+
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+}
+
+class _Loader extends StatelessWidget {
+  const _Loader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 49,
+      height: 49,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color(0xFFFE3D3D),
+      ),
+      clipBehavior: Clip.hardEdge,
+      padding: const EdgeInsets.all(11),
+      child: CircularProgressIndicator(
+        color: Colors.white,
       ),
     );
   }
