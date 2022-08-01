f24bfb3836571273e4bdc14e44467c1be11c38c0
artem-zaitsev <zaitsev_art@inbox.ru>
2022-08-01 16:59:42 +0300

    add base widgets

--- lib/src/features/animations/ui/play/play_screen.dart
+++ lib/src/features/animations/ui/play/play_screen.dart
@@ -16,8 +16,15 @@ class _ButtonScreenState extends State<PlayScreen> {
   }
 }
 
-class _Play extends StatelessWidget {
+class _Play extends StatefulWidget {
   const _Play({Key? key}) : super(key: key);
+
+  @override
+  State<_Play> createState() => _PlayState();
+}
+
+class _PlayState extends State<_Play> {
+  bool _isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
@@ -37,10 +44,39 @@ class _Play extends StatelessWidget {
       child: Material(
         type: MaterialType.transparency,
         child: InkWell(
-          onTap: () {},
+          onTap: () {
+            setState(() {
+              _isPlaying = !_isPlaying;
+            });
+          },
           child: Center(
-            child: Image.asset('assets/images/img_play.png'),
+            child: _isPlaying
+                ? _Stop()
+                : Image.asset('assets/images/img_play.png'),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Stop extends StatelessWidget {
+  const _Stop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 54,
+      height: 54,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFFFE3D3D),
+            Color(0xFF8DEBE5),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
       ),
     );
